8e0ec79d0436616a772f106a34122eb1290cc32c
mingyu <rlaalsrbgk@gmail.com>
2018-05-28 16:54:13 +0900

    정규표현식으로 anchoor태그의 내용을 파싱

--- re_sample.py
+++ re_sample.py
@@ -1,4 +1,17 @@
-with open('re_weekday_item_title.html', 'rt') as f:
+import re
+
+with open('weekday.html', 'rt') as f:
     html = f.read()
 
-print(html)
+#print(html)
+
+p = re.compile(r'<a.*?>(.*?)</a>')
+p = re.compile(r'''<a                      # <a로 시작하며
+                   .*?class="title".*?>    # class="title"문장이 들어있고
+                                           #>가 등장하기 전까지의 임의의 문자 최소 반복, >까지
+                   (.*?)                   # 임의의 문자 반복
+                   </a>                    # </a>가 나오기 전까지''', re.VERBOSE)
+
+m = re.findall(p, html)
+
+print(m)
