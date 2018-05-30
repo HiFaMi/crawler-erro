a6b0cfd434f3afa16010d4ffd5dd66bad7e61c4b
mingyu <rlaalsrbgk@gmail.com>
2018-05-30 20:39:54 +0900

    parsing_homework pycharm version

---
+++ parsing_homework.py
@@ -0,0 +1,159 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+class Episode:
+
+    def __init__(self, webtoon_id, no, url_thumbnail, title, rating, created_date):
+        self.webtoon_id = webtoon_id
+        self.no = no
+        self.url_thumbnail = url_thumbnail
+        self.title = title
+        self.rating = rating
+        self.created_date = created_date
+        self.image_list = []
+
+    @property
+    def info(self):
+        from urllib.parse import urlencode
+
+        payload = {'titleId': self.webtoon_id, 'no': self.no}
+        url = 'http://comic.naver.com/webtoon/detail.nhn?'
+
+        result = url + urlencode(payload)
+        return result
+
+
+class Webtoon:
+
+    def __init__(self, webtoon_id):
+
+        self.webtoon_id = webtoon_id
+        self.title = None
+        self.author = None
+        self.description = None
+        self.episode_list = []
+
+    def rework(self):
+
+        payload = {'titleId': self.webtoon_id}
+        if not os.path.exists('data/{}.html'.format(self.webtoon_id)):
+            toon_url = requests.get('http://comic.naver.com/webtoon/list.nhn', params=payload)
+            with open('data/{}.html'.format(self.webtoon_id), 'wt') as f:
+                f.write(toon_url.text)
+
+        with open('data/{}.html'.format(self.webtoon_id), 'rt') as f:
+            html = f.read()
+
+        soup = BeautifulSoup(html, 'lxml')
+        title_list = soup.select('div.detail > h2')
+        self.title = title_list[0].contents[0].strip()
+
+        auth_list = soup.select('span."wrt_nm"')
+        self.author = auth_list[0].string.strip()
+
+        self.description = soup.p.string
+
+    def update(self):
+        payload = {'titleId': self.webtoon_id}
+        if not os.path.exists('data/{}.html'.format(self.webtoon_id)):
+            toon_url = requests.get('http://comic.naver.com/webtoon/list.nhn', params=payload)
+            with open('data/{}.html'.format(self.webtoon_id), 'wt') as f:
+                f.write(toon_url.text)
+
+        with open('data/{}.html'.format(self.webtoon_id), 'rt') as f:
+            html = f.read()
+        soup = BeautifulSoup(html, 'lxml')
+        # image_url
+        list_src = soup.select("a > img['src']")
+
+        # 각 화의 제목
+        list_of_title = soup.select('td.title > a')
+
+        # 별점 리스트
+        rating_list = soup.select('div.rating_type > strong')
+
+        # 등록일
+        date_list = soup.select('td.num')
+
+        # no요소를 빈 리스트 안에 넣은 후 반환
+        # 값이 두번씩 반환되기에 step을 두번씩 주어서 반환
+        finall_list = []
+        no_list = soup.select('td.title > a[href]')
+        for i in range(0, len(no_list)):
+            a = no_list[i].get('href')
+            finall_list.append(re.findall(r'no=(.*?)&', a)[0])
+
+        for i in range(len(list_of_title)):
+            inst = Episode(self.webtoon_id, finall_list[i], list_src[i + 1].get('src'), list_of_title[i].string,
+                           rating_list[i].string, date_list[i].string)
+            self.episode_list.append(inst)
+
+    @classmethod
+    def search_webtoon(cls, webtoon_name):
+
+        if not os.path.exists('data/all_webtoon.html'):
+            all_webtoon_url = requests.get('http://comic.naver.com/webtoon/weekday.nhn')
+            with open('data/all_webtoon.html', 'wt') as f:
+                f.write(all_webtoon_url.text)
+
+        with open('data/all_webtoon.html', 'rt') as f:
+            html = f.read()
+
+        soup = BeautifulSoup(html, 'lxml')
+
+        all_webtoon_list = soup.select('div.col_inner > ul > li > a')
+        all_dict_webtoon_list = []
+
+        for i in range(len(all_webtoon_list)):
+            href = all_webtoon_list[i]['href']
+            titleId = re.findall('titleId=(.*?)&.*?', href)
+            (all_dict_webtoon_list).append({'Title': all_webtoon_list[i].string, 'titleId': titleId[0]})
+
+        search_list = []
+        for i in range(len(all_dict_webtoon_list)):
+            if webtoon_name in all_dict_webtoon_list[i]['Title']:
+                search_list.append(all_dict_webtoon_list[i]['Title'])
+        search_list = list(set(search_list))
+
+        while True:
+            for index, title in enumerate(search_list):
+                print('{}. {}'.format(index + 1, title))
+
+            user_input = input('선택: ')
+            webtoon = search_list[int(user_input) - 1]
+            break
+
+        for i in range(len(all_dict_webtoon_list)):
+            if webtoon == all_dict_webtoon_list[i]['Title']:
+                # user는 classmethod에서의 생성자 호출 변수
+                return cls((all_dict_webtoon_list[i]['titleId']))
+
+
+class EpisodeImage:
+    def __init__(self, episode, url):
+        self.episode = episode
+        self.url = url
+        self.image_list = []
+
+    def image_crawler(self, episode_user):
+
+        if not os.path.exists('data/{}.html'.format(episode_user)):
+            toon_url = requests.get(self.url)
+            with open('data/{}.html'.format(episode_user), 'wt') as f:
+                f.write(toon_url.text)
+
+        with open('data/{}.html'.format(episode_user), 'rt') as f:
+            html = f.read()
+
+        soup = BeautifulSoup(html, 'lxml')
+        list_src = soup.select('div.wt_viewer > img')
+
+        user = EpisodeImage(self.episode, self.url)
+
+        for i in range(len(list_src)):
+            (user.image_list).append(list_src[i]['src'])
+
+        (episode_user.image_list).append(user)
