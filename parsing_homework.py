fe65a4cf2a89c45a9b2a78e41e966ddd2edfe6d1
mingyu <rlaalsrbgk@gmail.com>
2018-05-31 23:20:01 +0900

    download method 및 주석추가

--- parsing_homework.py
+++ parsing_homework.py
@@ -17,13 +17,69 @@ class Episode:
 
     @property
     def info(self):
+        # urlencode방식은 여기서 밖에 안쓰게 되니 info에서만 import
         from urllib.parse import urlencode
 
         payload = {'titleId': self.webtoon_id, 'no': self.no}
         url = 'http://comic.naver.com/webtoon/detail.nhn?'
-
+        # urlencode를 통해 payload의 값이 key1=value1&key2=value2값으로 나오게 됨
+        # url문자열과 urlencode를 통해 합쳐지 값을 더함다.
+        # 문자열 + 문자열
         result = url + urlencode(payload)
         return result
+
+    def get_image_url_list(self):
+        # 그냥 함수가 실행 된 것을 알려줌
+        print('get_image-url_list_start')
+        # 파일 경로
+        file_path = 'data/episode_detail-{}-{}.html'.format(self.webtoon_id, self.no)
+        # file_path 값을 이용하여 만약 해당 경로에 값이 없을경우 파일을 만들어주고
+        # 그 안에 self.info의 값을 가져와 requests.get을 이용하여
+        # 해당 url의 text(태그 및 속성)값을 가져와서 적고 저장
+        # 해당경로 파일 존재시 읽어오기
+        if not os.path.exists(file_path):
+            url = requests.get(self.info)
+            with open('{}'.format(file_path), 'wt') as f:
+                f.write(url.text)
+        with open('{}'.format(file_path), 'rt') as f:
+            html = f.read()
+
+        soup = BeautifulSoup(html, 'lxml')
+        # img_list는 BeautifulSoup을 이용하여 해당 태그 안에 있는 문자를 가져온다 -> 리스트 형식
+        img_list = soup.select('div.wt_viewer > img')
+        # img_list중 하나하나의 값을 img로 가져온 후 그 안의 'src' 값을 반환
+        return [img.get('src') for img in img_list]
+
+    def download_all_images(self):
+        # download method를 이용하여 get_image_url_list method에 반환값 즉, src의 값을 url에 넣고 실행
+        for url in self.get_image_url_list():
+            self.download(url)
+
+    def download(self, url_img):
+        # Referer: 이전 페이지 URL(어떤 페이지를 거쳐서 왔는가?)
+        # 몇몇 사이트는 Referer 헤더 값을 보고 응답을 거부하기도 함
+        # 따라서 Referer 값을 커스텀 해 주어야 한다.
+        url_referer = 'http://comic.naver.com/webtoon/list.nhn?titledId={}'.format(self.webtoon_id)
+        headers = {
+            'Referer': url_referer
+        }
+
+        response = requests.get(url_img, headers=headers)
+
+        # '/'을 기준으로 1번만 split을 하는데 오른쪽 부터 'r(right)'split 값중 마지막 값
+        file_name = url_img.rsplit('/', 1)[-1]
+        dir_path = 'data/{}/{}'.format(self.webtoon_id, self.no)
+
+        # os.mkdir(path) = <path>에 해당하는 디렉터리를 생성합니다.
+        # os.makedirs(path) = 인자로 전달된 디렉터리를 재귀적으로 생성
+        # 이미 디렉터리가 생성되어 있는 경우나 권한이 없어 생성할 수 없는 경우는 예외를 발생합니다.
+        os.makedirs(dir_path, exist_ok=True)
+
+        file_path = '{}/{}'.format(dir_path, file_name)
+
+        # wb는 이진파일 쓰기 전용
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
 
 
 class Webtoon:
@@ -48,6 +104,8 @@ class Webtoon:
             html = f.read()
 
         soup = BeautifulSoup(html, 'lxml')
+
+        # 각각의 태그 안에 있는 내용들을 BeautifulSoup을 통해 파싱
         title_list = soup.select('div.detail > h2')
         self.title = title_list[0].contents[0].strip()
 
@@ -79,13 +137,14 @@ class Webtoon:
         date_list = soup.select('td.num')
 
         # no요소를 빈 리스트 안에 넣은 후 반환
-        # 값이 두번씩 반환되기에 step을 두번씩 주어서 반환
         finall_list = []
         no_list = soup.select('td.title > a[href]')
-        for i in range(0, len(no_list)):
+        for i in range(len(no_list)):
             a = no_list[i].get('href')
+            # no값만 가져오기 위하여 href값 안에서 정규표현식을 이용하여 파싱
             finall_list.append(re.findall(r'no=(.*?)&', a)[0])
 
+        # 리스트에 값이 아닌 클래스 생성자를 넣음
         for i in range(len(list_of_title)):
             inst = Episode(self.webtoon_id, finall_list[i], list_src[i + 1].get('src'), list_of_title[i].string,
                            rating_list[i].string, date_list[i].string)
@@ -105,19 +164,23 @@ class Webtoon:
         soup = BeautifulSoup(html, 'lxml')
 
         all_webtoon_list = soup.select('div.col_inner > ul > li > a')
-        all_dict_webtoon_list = []
-
+        dict_list = []
+        # 모든 웹툰 제목과 그에 해당하는 titleId를 파싱하여 딕트 형태로 만듬
         for i in range(len(all_webtoon_list)):
             href = all_webtoon_list[i]['href']
             titleid = re.findall('titleId=(.*?)&.*?', href)
-            all_dict_webtoon_list.append({'Title': all_webtoon_list[i].string, 'titleId': titleid[0]})
+            dict_list.append({'Title': all_webtoon_list[i].string, 'titleId': titleid[0]})
 
         search_list = []
-        for i in range(len(all_dict_webtoon_list)):
-            if webtoon_name in all_dict_webtoon_list[i]['Title']:
-                search_list.append(all_dict_webtoon_list[i]['Title'])
+        # 찾으려는 웹툰의 부분적인 문자를 받아 그 문자가 포함되어 있는 모든 파일을 혹시 모를 중복을 없게 만들고
+        # set을 사용한 경우 딕트 값으로 바뀌기 떄문에 list화 시켜준다.
+        for i in range(len(dict_list)):
+            if webtoon_name in dict_list[i]['Title']:
+                search_list.append(dict_list[i]['Title'])
         search_list = list(set(search_list))
 
+        # 해당 결과에 해당하는 리스트를 보여주고 찾으려는 웹튼의 번호를 적게 되면
+        # webtoon에게는 선택 된 값이 주어진다.
         while True:
             for index, title in enumerate(search_list):
                 print('{}. {}'.format(index + 1, title))
@@ -125,20 +188,18 @@ class Webtoon:
             user_input = input('선택: ')
             webtoon = search_list[int(user_input) - 1]
             break
-
-        for i in range(len(all_dict_webtoon_list)):
-            if webtoon == all_dict_webtoon_list[i]['Title']:
-                # user는 classmethod에서의 생성자 호출 변수
-                return cls((all_dict_webtoon_list[i]['titleId']))
+        # webtoon에 주어진 값을 이용해 위에서 만든 딕트 타입과 비교하여
+        # titleId를 반환하고 생성자를 만들어 준다.
+        for i in range(len(dict_list)):
+            if webtoon == dict_list[i]['Title']:
+                return cls((dict_list[i]['titleId']))
 
 
 class EpisodeImage:
-
     def __init__(self, episode, url):
-
         self.episode = episode
         self.url = url
-        self.image_list = []
+        self.image_url_list = []
 
     def image_crawler(self, episode_user):
 
@@ -151,11 +212,12 @@ class EpisodeImage:
             html = f.read()
 
         soup = BeautifulSoup(html, 'lxml')
+        # 아래의 태그에 해당하는 태그를 파싱
         list_src = soup.select('div.wt_viewer > img')
-
+        # 생성자를 만들어 준다
         user = EpisodeImage(self.episode, self.url)
-
+        # 생성자의 리스트에 list_src의 태그 안에서 'src' 값만 넘겨준다.
         for i in range(len(list_src)):
-            user.image_list.append(list_src[i]['src'])
+            user.image_url_list.append(list_src[i]['src'])
 
         episode_user.image_list.append(user)
